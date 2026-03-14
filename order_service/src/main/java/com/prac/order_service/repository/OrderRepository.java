ac03c990f8b9de630cf1568cad59a2b3774da6ad
Yash Sindhu <yashsindhu123@gmail.com>
2026-03-14 21:46:08 +0530

    Delete order_service directory

--- order_service/src/main/java/com/prac/order_service/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prac.order_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prac.order_service.entity.BookOrder;
-
-public interface OrderRepository extends JpaRepository<BookOrder, Long> {
-
-}
